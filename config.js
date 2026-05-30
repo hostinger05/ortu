75f0e034dc68a014ad4e16e7c4b87f9c51e99d68
hostinger05 <hostinggogo5@gmail.com>
2026-05-30 21:52:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ window.GITHUB_CONFIG = {
   owner:  "hostinger05",
   repo:   "ortu",
   branch: "main",
-  token:  "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",        // ← isi token GitHub (opsional, untuk sync file JS)
+  token:  "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",        // ← isi token GitHub (opsional, untuk sync file JS)
 
   // URL Apps Script Web App — wajib diisi untuk fitur ganti password
   gasUrl: "https://script.google.com/macros/s/AKfycby3_7Wg5LJGAbZLizbdSFe4oMKspA0U954WR9ue3eTXsBtOYuLwun_S2W7Y_8XdAjKinA/exec",        // ← contoh: "https://script.google.com/macros/s/AKfyc.../exec"
